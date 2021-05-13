1356a55a73003b2914f80bba4e095fea19f2d6a8
CdreamE <48158862+CdreamE@users.noreply.github.com>
2021-05-14 00:31:44 +0800

    Add files via upload

---
+++ App/music/module/song/state/BaseState.java
@@ -0,0 +1,25 @@
+package com.example.q.pocketmusic.module.song.state;
+
+import com.example.q.pocketmusic.config.Constant;
+import com.example.q.pocketmusic.model.bean.Song;
+import com.example.q.pocketmusic.model.bean.ask.AskSongComment;
+
+
+//状态基类
+public class BaseState {
+    private Song song;
+    private int loadingWay;
+
+    public BaseState(Song song, int loadingWay) {
+        this.song = song;
+        this.loadingWay = loadingWay;
+    }
+
+    public Song getSong() {
+        return song;
+    }
+
+    public int getLoadingWay() {
+        return loadingWay;
+    }
+}
---
+++ App/music/module/song/state/SearchState.java
@@ -0,0 +1,41 @@
+package com.example.q.pocketmusic.module.song.state;
+
+import android.content.Context;
+
+import com.example.q.pocketmusic.config.Constant;
+import com.example.q.pocketmusic.model.bean.Song;
+import com.example.q.pocketmusic.model.net.LoadSearchSongPic;
+import com.example.q.pocketmusic.module.song.SongActivityPresenter;
+
+
+//搜索状态
+public class SearchState extends BaseState implements IState {
+    private Context context;
+    private SongActivityPresenter.IView activity;
+
+    public SearchState(Song song, Context context, SongActivityPresenter.IView activity) {
+        super(song, Constant.NET);
+        this.context = context;
+        this.activity = activity;
+    }
+
+    @Override
+    public void loadPic() {
+        new LoadSearchSongPic() {
+            @Override
+            protected void onPostExecute(Integer integer) {
+                super.onPostExecute(integer);
+                setLoadIntResult(integer);
+            }
+        }.execute(getSong());
+    }
+
+    private void setLoadIntResult(int result) {
+        if (result == Constant.FAIL) {
+            activity.loadFail();
+        } else {
+            activity.setPicResult(getSong().getIvUrl(), getLoadingWay());
+        }
+    }
+
+}
