2f9ce59dcc4b929d34d50d7409ece30ac6e64a94
CdreamE <48158862+CdreamE@users.noreply.github.com>
2021-05-14 00:24:44 +0800

    Add files via upload

---
+++ App/music/view/widget/net/Element.java
@@ -0,0 +1,16 @@
+package com.example.q.pocketmusic.view.widget.net;
+
+
+public class Element {
+    public int color;
+    public Double direction;
+    public float speed;
+    public float x = 0;
+    public float y = 0;
+
+    public Element(int color, Double direction, float speed){
+        this.color = color;
+        this.direction = direction;
+        this.speed = speed;
+    }
+}
